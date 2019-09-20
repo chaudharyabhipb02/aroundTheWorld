81f1216e2eefc6b4bef693a7f5352e026055b936
kunal <kunalgupta9607@gmail.com>
2019-09-20 16:44:25 +0530

    Fixed PEP8 styles and indentations

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls import url
 from. import views
-urlpatterns=[url(r'pro',views.pro,name='pro'),
-             url(r'about',views.about,name='about'),
-             url(r'destination', views.destination, name='destination'),
-             url(r'contacts', views.contact, name='contacts'),
-             url(r'tour', views.tour, name='tour'),
-             url(r'home', views.home, name='home'),
-             url(r'register', views.register, name='register'),
-             url(r'login', views.login, name='login'),
-             url(r'sss', views.sis, name='sis'),
 
-
-
-            ]
+urlpatterns = [
+    url(r'pro', views.pro, name='pro'),
+    url(r'about', views.about, name='about'),
+    url(r'destination', views.destination, name='destination'),
+    url(r'contacts', views.contact, name='contacts'),
+    url(r'tour', views.tour, name='tour'),
+    url(r'home', views.home, name='home'),
+    url(r'register', views.register, name='register'),
+    url(r'login', views.login, name='login'),
+    url(r'sss', views.sis, name='sis'),
+]
